f9fbbda4f6659a6e7bbca34c04f0c3a83bceea3e
sergiot <sergiotorres81@gmail.com>
2016-02-25 00:26:09 +0200

    changed how to manage the POST request

--- src/main/java/com/st/controller/AccountController.java
+++ src/main/java/com/st/controller/AccountController.java
@@ -3,8 +3,11 @@ package com.st.controller;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,10 +26,14 @@ public class AccountController {
 		return accountRepository.findAll();
 	}
 
-	@RequestMapping(value = "/api/accounts/{name}", method = RequestMethod.POST)
-	public Account addAccount(@PathVariable String name) {
-		Account entity = new Account(name, new Date());
-		return accountRepository.save(entity);
+	@RequestMapping(value = "/api/accounts", method = RequestMethod.POST)
+	public ResponseEntity<Account> addAccount(@RequestBody Account account) {
+		if (StringUtils.isEmpty(account.getName())) {
+			return new ResponseEntity<Account>(HttpStatus.BAD_REQUEST);
+		}
+		Account entity = new Account(account.getName(), new Date());
+		ResponseEntity<Account> response = new ResponseEntity<>(accountRepository.save(entity), HttpStatus.OK);
+		return response;
 	}
 
 }
